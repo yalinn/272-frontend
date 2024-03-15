7ff98ee2e0bbe9d3c7a2cedaba11dc2aa345ef44
Orhan Yalin ozturk <orhanyalin@hotmail.com>
2024-03-15 04:43:06 +0300

    error fix in deployment

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const config = {
     PORT: "3000",
-    PUBLIC_URL: "https://probee.yalin.app/",
+    PUBLIC_URL: "https://probee.yalin.app",
     WEBSITE_VERSION: '0.0.1',
-    API_ENDPOINT: "https://api-probee.yalin.app/",
+    API_ENDPOINT: "https://api-probee.yalin.app",
     API_PORT: "8080",
     WS_ENDPOINT: "wss://ws-probee.yalin.app/",
     WS_PORT: '3500',
