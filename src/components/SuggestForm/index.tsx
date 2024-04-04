c203f4e801e6ba07af4c31e594dc5eddd79daa8a
Orhan Yalin ozturk <orhanyalin@hotmail.com>
2024-04-04 13:03:56 +0300

    form loading

--- src/components/SuggestForm/index.tsx
+++ src/components/SuggestForm/index.tsx
@@ -9,6 +9,7 @@ const langs = {
     submit: "Submit",
     no_title: "Please enter a title",
     no_suggestion: "Please enter a suggestion",
+    success_submit: "Submit successful!",
   },
   tr: {
     title: "Başlık",
@@ -16,11 +17,13 @@ const langs = {
     submit: "Gönder",
     no_title: "Lütfen bir başlık girin",
     no_suggestion: "Lütfen bir öneri girin",
+    success_submit: "Gönderme başarılı!",
   },
 };
 
 import { SubmitHandler, useForm } from "react-hook-form";
 import { Button } from "@/lib/ui/button";
+import { cn } from "@/lib/utils";
 const SuggestForm = ({
   language,
   setOpen,
@@ -37,22 +40,31 @@ const SuggestForm = ({
     clearErrors,
   } = useForm({ mode: "onChange" });
   const [errs, setErrs] = React.useState<string[]>([]);
+  const [loading, setLoading] = React.useState(false);
+  const [err, setErr] = React.useState<string | null>(null);
   const onSubmit: SubmitHandler<any> = async (data) => {
+    setLoading(true);
     const res = await fetch(`/api/suggestions`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
-    }).then((res) => res.json());
+    }).then((res) => {
+      setLoading(false);
+      return res.json();
+    });
     if (res.length) {
       setOpen(false);
+      setErr("");
       document.location.reload();
     }
     if (Object.keys(res).length !== 0) {
       setErrs(() => Object.keys(res));
+      setErr(res.error);
     } else {
       setOpen(false);
+      setErr("");
     }
   };
   const lang = language === "en" ? langs.en : langs.tr;
@@ -75,7 +87,7 @@ const SuggestForm = ({
         <Form.Control asChild>
           <textarea
             className="bg-white/10 border w-full border-white/20 rounded-md px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-            {...register("title", { required: true, maxLength: 50})}
+            {...register("title", { required: true, maxLength: 50 })}
           />
         </Form.Control>
       </Form.Field>
@@ -96,7 +108,7 @@ const SuggestForm = ({
         <Form.Control asChild required={true}>
           <textarea
             className="bg-white/10 border w-full border-white/20 rounded-md px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-            {...register("content", { required: true, maxLength: 50})}
+            {...register("content", { required: true, maxLength: 50 })}
           />
         </Form.Control>
       </Form.Field>
@@ -104,8 +116,19 @@ const SuggestForm = ({
         className="w-full bg-blue-500 text-white rounded-md p-2"
         asChild
       >
-        <Button type="submit" onClick={handleSubmit(onSubmit)}>
-          {lang.submit}
+        <Button
+          type="submit"
+          className={cn(
+            loading || err == "" ? "cursor-not-allowed" : "cursor-pointer",
+            "rounded-xl transition-colors duration-300 ease-in-out"
+          )}
+          onClick={handleSubmit(onSubmit)}
+        >
+          {loading ? (
+            <div id="loading" className="w-8 h-8"></div>
+          ) : (
+            <div>{err == "" ? lang.success_submit : lang.submit}</div>
+          )}
         </Button>
       </Form.Submit>
     </Form.Root>
